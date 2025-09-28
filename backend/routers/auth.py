2c74902d38bc93a7620e2d2ed98ef7305b7c5a3f
Koses Suvarnasuddhi <koses.su@gmail.com>
2025-09-28 14:29:20 +0700

    Login and Signup API Endpoint with jwt

---
+++ backend/routers/auth.py
@@ -0,0 +1,66 @@
+from pydantic import BaseModel
+from model.user import User
+from config.db import users_collection
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status, APIRouter
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import jwt
+import os
+from dotenv import load_dotenv
+from schema.schemas import individual_serialize
+from utils.security import hash_password, verify_password
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+    email: str = None
+    full_name: str = None
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register(userReq: CreateUserRequest):
+    if users_collection.find_one({"username": userReq.username}):
+        raise HTTPException(status_code=400, detail="Username already exists")
+    hashed_password = hash_password(userReq.password)
+    user_dict = {
+        "username": userReq.username,
+        "hashed_password": hashed_password,
+        "email": userReq.email,
+        "full_name": userReq.full_name
+    }
+    users_collection.insert_one(user_dict)
+    return {"msg": "User created successfully"}
+
+@router.post("/token", response_model=Token)
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = individual_serialize(users_collection.find_one({"username": form_data.username}))
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token = create_access_token(user["username"], user["id"], timedelta(minutes=60))
+    return {"access_token": token, "token_type": "bearer"}
+
+def create_access_token(username: str, user_id: str, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
